b1783ec3877e0561648c388a48a717b07e445157
Daniel Segura <quirkyexplorer@gmail.com>
2025-12-10 14:07:29 -0700

    commented out autoplay feature

--- src/app/components/CarouselAutoplay.tsx
+++ src/app/components/CarouselAutoplay.tsx
@@ -1,57 +1,57 @@
-"use client"
-import { useCallback, useEffect, useState } from 'react'
-import { EmblaCarouselType } from 'embla-carousel'
+// "use client"
+// import { useCallback, useEffect, useState } from 'react'
+// import { EmblaCarouselType } from 'embla-carousel'
 
-type UseAutoplayType = {
-  autoplayIsPlaying: boolean
-  toggleAutoplay: () => void
-  onAutoplayButtonClick: (callback: () => void) => void
-}
+// type UseAutoplayType = {
+//   autoplayIsPlaying: boolean
+//   toggleAutoplay: () => void
+//   onAutoplayButtonClick: (callback: () => void) => void
+// }
 
-export const useAutoplay = (
-  emblaApi: EmblaCarouselType | undefined
-): UseAutoplayType => {
-  const [autoplayIsPlaying, setAutoplayIsPlaying] = useState(false)
+// export const useAutoplay = (
+//   emblaApi: EmblaCarouselType | undefined
+// ): UseAutoplayType => {
+//   const [autoplayIsPlaying, setAutoplayIsPlaying] = useState(false)
 
-  const onAutoplayButtonClick = useCallback(
-    (callback: () => void) => {
-      const autoplay = emblaApi?.plugins()?.autoplay
-      if (!autoplay) return
+//   const onAutoplayButtonClick = useCallback(
+//     (callback: () => void) => {
+//       const autoplay = emblaApi?.plugins()?.autoplay
+//       if (!autoplay) return
 
-      const resetOrStop =
-        autoplay.options.stopOnInteraction === false
-          ? autoplay.reset
-          : autoplay.stop
+//       const resetOrStop =
+//         autoplay.options.stopOnInteraction === false
+//           ? autoplay.reset
+//           : autoplay.stop
 
-      resetOrStop()
-      callback()
-    },
-    [emblaApi]
-  )
+//       resetOrStop()
+//       callback()
+//     },
+//     [emblaApi]
+//   )
 
-  const toggleAutoplay = useCallback(() => {
-    const autoplay = emblaApi?.plugins()?.autoplay
-    if (!autoplay) return
+//   const toggleAutoplay = useCallback(() => {
+//     const autoplay = emblaApi?.plugins()?.autoplay
+//     if (!autoplay) return
 
-    const playOrStop = autoplay.isPlaying() ? autoplay.stop : autoplay.play
-    playOrStop()
-  }, [emblaApi])
+//     const playOrStop = autoplay.isPlaying() ? autoplay.stop : autoplay.play
+//     playOrStop()
+//   }, [emblaApi])
 
-  useEffect(() => {
-    const autoplay = emblaApi?.plugins()?.autoplay
-    if (!autoplay) return
+//   useEffect(() => {
+//     const autoplay = emblaApi?.plugins()?.autoplay
+//     if (!autoplay) return
 
-    setAutoplayIsPlaying(autoplay.isPlaying())
-    emblaApi
-      .on('autoplay:play', () => setAutoplayIsPlaying(true))
-      .on('autoplay:stop', () => setAutoplayIsPlaying(false))
-      .on('reInit', () => setAutoplayIsPlaying(autoplay.isPlaying()))
-  }, [emblaApi])
+//     setAutoplayIsPlaying(autoplay.isPlaying())
+//     emblaApi
+//       .on('autoplay:play', () => setAutoplayIsPlaying(true))
+//       .on('autoplay:stop', () => setAutoplayIsPlaying(false))
+//       .on('reInit', () => setAutoplayIsPlaying(autoplay.isPlaying()))
+//   }, [emblaApi])
 
-  return {
-    autoplayIsPlaying,
-    toggleAutoplay,
-    onAutoplayButtonClick
-  }
-}
+//   return {
+//     autoplayIsPlaying,
+//     toggleAutoplay,
+//     onAutoplayButtonClick
+//   }
+// }
 
